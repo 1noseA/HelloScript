0fd410e4162530690dcdf489650ea64a4b64e656
1noseA <1noseA2L@gmail.com>
2021-10-23 15:48:46 +0900

    ボタン

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,6 +1,6 @@
 /* 変数宣言 */
-var msg = "Hello,JavaScript";
-var num = 10;
+/*var msg = "Hello,JavaScript";
+var num = 10;*/
 
 /* 配列はちょっと違う */
 /*var array = new Array(5);
@@ -9,23 +9,23 @@ array[1] = "B";
 array[2] = "C";*/
 
 /* 変数宣言と初期化 */
-var array = ["A", "B", "C"];
+/*var array = ["A", "B", "C"];*/
 
 /*document.writeln(msg);
 document.writeln(num + 1);*/
-document.writeln(array[2]);
+/*document.writeln(array[2]);*/
 
 /* lengthも同じ */
-document.writeln(array.length);
+/*document.writeln(array.length);*/
 
 /* deleteで要素を消せる */
-delete array[1];
+/*delete array[1];*/
 /* 宣言されていないので「undefined」と表示される */
-document.writeln(array[1]);
+/*document.writeln(array[1]);*/
 
 /* pushメソッドで要素を追加できる */
-array.push("D");
-document.writeln(array[3]);
+/*array.push("D");
+document.writeln(array[3]);*/
 
 /* 連想配列が使える（Mapみたい） */
 /*var week = new Array(5);
@@ -35,12 +35,12 @@ document.writeln(week["Mon"]);*/
 /* この書き方でもできる */
 /*document.writeln(week.Tue);*/
 
-var week = {
+/*var week = {
 	Mon: "Mondey",
 	Tue: "Tuesday"
 };
 document.writeln(week.Mon);
-
+*/
 /*var result = prompt("数字を入力してください", "");*/
 /* evalで数値にする */
 /*document.writeln(eval(result) + 1);*/
@@ -51,20 +51,20 @@ document.writeln(week.Mon);
 	document.writeln("奇数");
 }*/
 
-var num = [1,2,3,4,5];
+/*var num = [1,2,3,4,5];
 for (var i = 0; i < num.length; i++) {
 	document.writeln(num[i]);
 }
-
+*/
 /* 連想配列は別 */
-var numArray = {
+/*var numArray = {
 	a: "A",
 	b: "B",
 	c: "C"
 };
 for (var key in numArray) {
 	document.writeln(numArray[key]);
-};
+};*/
 
 /* 関数宣言の前でも後でもよし */
 /*sample();*/
@@ -74,8 +74,12 @@ for (var key in numArray) {
 }*/
 
 /* 戻り値 */
-function sample() {
+/*function sample() {
 	return "SAMPLE";
 }
 var result = sample();
-document.writeln(result);
+document.writeln(result);*/
+
+function sample() {
+	document.writeln("hello.");
+}
