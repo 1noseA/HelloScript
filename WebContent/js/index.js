f8577888977c4501658cd15bdb66c8fddb4d52a3
1noseA <1noseA2L@gmail.com>
2021-10-30 15:07:38 +0900

    Ajax

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,13 +1,14 @@
 window.onload = function() {
-	var updateLink = document.getElementById("update");
-	var target = document.getElementById("data1");
-	updateLink.onclick = function() {
-		var text = target.innerHTML;
-		var textBox = document.createElement("input");
-		textBox.setAttribute("type", "text");
-		textBox.setAttribute("value", text);
-
-		var parent = target.parentNode;
-		parent.replaceChild(textBox, target);
+	var btn = document.getElementById("btn");
+	btn.onclick = function() {
+		var xmlHttp = new XMLHttpRequest();
+		xmlHttp.onreadystatechange = function() {
+			if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+				var obj = JSON.parse(xmlHttp.responseText);
+				alert(obj.address);
+			}
+		}
+		xmlHttp.open("GET", "http://localhost:8080/HelloScript/sample");
+		xmlHttp.send(null);
 	}
 }
---
+++ src/com/sample/SampleSevlet.java
@@ -0,0 +1,42 @@
+package com.sample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SampleSevlet
+ */
+@WebServlet("/sample")
+public class SampleSevlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SampleSevlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().write("{\"name\" : \"sample\", \"age\" : 20, \"address\" : \"tokyo\"}");
+		response.getWriter().flush();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
